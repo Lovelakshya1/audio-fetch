61a7724a1b9f4c161e83b75e7151e6a13fecaf4a
kabirvvv <kabirv338@gmail.com>
2026-06-17 21:17:47 +0530

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.audiofetch"
         minSdk = 24
         targetSdk = 34
-        versionCode = 5.1
-        versionName = "5.1"
+        versionCode = 6
+        versionName = "6"
         ndk {
             abiFilters += listOf("arm64-v8a", "x86_64")
         }
