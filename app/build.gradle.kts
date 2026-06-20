8b70c3b871955c8f8686a247110c218f820333bc
Luv <yogirajpurohit08@gmail.com>
2026-06-20 18:21:04 +0530

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,10 +10,10 @@ android {
         applicationId = "com.audiofetch"
         minSdk = 24
         targetSdk = 34
-        versionCode = 9
-        versionName = "9"
+        versionCode = 10
+        versionName = "10"
         ndk {
-            abiFilters += listOf("arm64-v8a", "x86_64")
+            abiFilters += listOf("arm64-v8a")
         }
     }
     // --- NEW: signing config for release builds ---
